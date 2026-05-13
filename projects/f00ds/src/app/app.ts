42e7ff57b11df3376e7dd1dd086306cb13d160bc
Letizia Marino <ltzmrn@gmail.com>
2026-05-13 16:47:42 +0200

    f00ds Firebase init

--- projects/f00ds/src/app/app.ts
+++ projects/f00ds/src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ThemeService } from 'shared-core';
+import { FirebaseService } from 'shared-firebase'
+import { FIREBASE_CONFIG } from "../environments/environments";
 
 @Component({
   selector: "app-root",
@@ -11,7 +13,8 @@ import { ThemeService } from 'shared-core';
 export class App {
   protected readonly title = signal("f00ds");
 
-  constructor(private readonly themeService: ThemeService) {
-    this.themeService.init()
+  constructor(private readonly themeService: ThemeService, private firebaseService: FirebaseService) {
+    this.themeService.init();
+    this.firebaseService.init(FIREBASE_CONFIG);
   }
 }
---
+++ projects/f00ds/src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBDQp0wQ9V6NKuolRwKZbGWH8t_1Zyja04",
+    authDomain: "b00ks2024.firebaseapp.com",
+    projectId: "b00ks2024",
+    storageBucket: "b00ks2024.firebasestorage.app",
+    messagingSenderId: "340717401984",
+    appId: "1:340717401984:web:39a00264900ac61b43f365"
+}
